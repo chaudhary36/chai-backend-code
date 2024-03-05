36a138a08e20b3d288054ee358d649f904ec0755
Anuj Choudhary <anujchaudhari2222@gmail.com>
2024-03-04 18:38:14 -0800

    watch history aggregation pipelines

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generatorAccessAndRefreshToken = async (userId) => {
   try {
@@ -396,9 +397,63 @@ const findUserChannel = asyncHandler(async (req, res) => {
   res
   .status(200)
   .json(
-    new ApiResponse(200 , channel , "Channel is fetched successfully !!")
+    new ApiResponse(200 , channel[0] , "Channel is fetched successfully !!")
   )
 
+})
+
+const UserWatchHistory = asyncHandler(async(req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner"
+              }
+            }
+          }
+        ]
+      }
+    },
+  ])
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200,
+      user[0].watchHistory,
+      "Watch History fetched successfully!!"
+    )
+  )
 })
 
 export {
@@ -411,4 +466,6 @@ export {
   updateUserDetails,
   updateUserAvatar,
   UpdateUserCoverImage,
+  findUserChannel,
+  UserWatchHistory
 };
