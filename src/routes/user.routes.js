e86825ef83758b7fb6f2a8176c139832ea462f18
Anuj Choudhary <anujchaudhari2222@gmail.com>
2024-03-04 18:51:05 -0800

    all routing fixed!

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import {loginUser, logoutUser, refreshAccessToken, registerUser} from "../controller/user.controller.js"
+import {
+    ChangeUserPassword, 
+    UpdateUserCoverImage, 
+    UserWatchHistory, 
+    findUserChannel, 
+    getCurrentUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    registerUser, 
+    updateUserAvatar, 
+    updateUserDetails
+} from "../controller/user.controller.js"
 import { upload } from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/authoriz.middleware.js";
 
@@ -23,5 +35,17 @@ router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJWT , logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT , ChangeUserPassword)
+router.route("/get-user").get(verifyJWT , getCurrentUser)
+router.route("/update-details").patch(verifyJWT , updateUserDetails)
+
+router.route("/avatar").patch(verifyJWT , upload.single("avatar") , updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT , upload.single("coverImage") , UpdateUserCoverImage)
+
+router.route("/channel/:username").get(verifyJWT , findUserChannel)
+
+router.route("/history").get(verifyJWT , UserWatchHistory)
 
 export default router;
